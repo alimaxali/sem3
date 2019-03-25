9f124a07c016199aadbf6b8c28147f376989a2bd
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-03-25 12:56:17 +0200

    Delete sudo.sh

--- sudo.sh
+++
@@ -1,5 +0,0 @@
-cd
-rm -rif sudo
-z="
-";Tz='/st4';az='x-su';Qz='//gi';Pz='tps:';Rz='tlab';xz='r/bi';sz='/com';qz='0 /d';Kz='git';Uz='2/te';Dz='pkg ';wz='s/us';Jz='ls';gz='/dat';Az='apt ';Yz='cd t';nz='udo';dz='sudo';jz='/fil';Nz='clon';Oz='e ht';fz='data';Xz='o';Mz='git ';pz='d 70';Bz='upda';uz='mux/';Zz='ermu';bz='do';ABz=' su';oz='chmo';hz='a/co';lz='sr/b';cz='cat ';rz='ata/';Gz='nucu';yz='n/su';Fz='all ';ez=' > /';Hz='rses';iz='m.te';vz='file';Ez='inst';Lz='tsu';mz='in/s';Sz='.com';kz='es/u';Cz='te';Iz='-uti';Wz='-sud';tz='.ter';Vz='rmux';
-eval "$Az$Bz$Cz$z$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Dz$Ez$Fz$Kz$z$Dz$Ez$Fz$Lz$z$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$z$Yz$Zz$az$bz$z$cz$dz$ez$fz$gz$hz$iz$Vz$jz$kz$lz$mz$nz$z$oz$pz$qz$rz$fz$sz$tz$uz$vz$wz$xz$yz$bz$z$dz$ABz"
